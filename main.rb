78cca8ec86af3b436a9bcd3da4b6024966322db8
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-10-06 13:30:45 -0400

    Complete Insert method

--- main.rb
+++ main.rb
@@ -1,36 +1,54 @@
 class Node
     include Comparable
     attr_accessor :data, :left, :right
-    def initialize(data=0, left=nil, right=nil)
+    def initialize(data)
         @data = data
-        @left = left
-        @right = right
+        @left = nil
+        @right = nil
     end
 
     def <=>(other_nodes)
         @data <=> other_nodes.data
     end
+
 end
 
 class Tree
-    def initialize(arr)
-        @arr = arr
-        @root = build_tree(@arr, 0, @arr.length - 1)
+    attr_accessor :root, :data
+    def initialize(array)
+        @data= array.uniq.sort
+        @root = build_tree(@data)
     end
 
-    def build_tree(an_array, start_tree, end_tree)
-        if start_tree > end_tree
-            return nil
-        end
+    def build_tree(an_array)
+        return nil if an_array.empty?
 
-        mid_tree = (start_tree + end_tree) / 2
+        mid_tree =  (an_array.length - 1) / 2
 
         root = Node.new(an_array[mid_tree])
 
-        root.left = build_tree(an_array, start_tree, mid_tree - 1)
-        root.right = build_tree(an_array, mid_tree + 1, end_tree)
+        root.left = build_tree(an_array[0...mid_tree])
+        root.right = build_tree(an_array[mid_tree + 1..-1])
 
         return root
     end
+
+    def insert(value, root=self.root)
+        
+        return Node.new(value) if root.nil?
+        
+        if root.data == value
+            return root.data
+        elsif root.data < value
+            root.right.nil? ? root.right = Node.new(value) : insert(value, root.right)
+        else
+            root.left.nil? ? root.left = Node.new(value) : insert(value, root.left)
+        end
+    end
 end
 
+
+binary_tree = Tree.new([1, 2, 3, 4, 5])
+binary_tree.insert(6)
+
+p binary_tree.root
