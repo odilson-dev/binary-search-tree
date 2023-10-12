bab2d7b34ca6ef79e0fe7e53b41d71d7bea56718
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-10-11 20:31:21 -0400

    Implement delete function

--- main.rb
+++ main.rb
@@ -45,10 +45,29 @@ class Tree
             root.left.nil? ? root.left = Node.new(value) : insert(value, root.left)
         end
     end
+
+    def delete(value, node = root)
+        return node if node.nil?
+    
+        if value < node.data
+          node.left = delete(value, node.left)
+        elsif value > node.data
+          node.right = delete(value, node.right)
+        else
+          # if node has one or no child
+          return node.right if node.left.nil?
+          return node.left if node.right.nil?
+    
+          # if node has two children
+          leftmost_node = leftmost_leaf(node.right)
+          node.data = leftmost_node.data
+          node.right = delete(leftmost_node.data, node.right)
+        end
+        node
+    end
 end
 
 
 binary_tree = Tree.new([1, 2, 3, 4, 5])
-binary_tree.insert(6)
-
+binary_tree.delete(2)
 p binary_tree.root
